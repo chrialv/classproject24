571aa9dfe98b2c4a23705584c905dd1ce9989171
chrialv <alvarez_chris@live.com>
2024-10-28 00:24:44 +0000

    added password characters etc

--- app/src/pages/Signup.jsx
+++ app/src/pages/Signup.jsx
@@ -6,9 +6,77 @@ import Col from 'react-bootstrap/Col';
 
 import { createUserWithEmailAndPassword } from 'firebase/auth';
 import { useNavigate } from "react-router-dom"
+import { useState, useEffect } from 'react';
 
 export function Signup(props) {
+    const [ password, setPassword ] = useState("")
+    const [ validpassword, setValidPassword ] = useState( false )
+
     const navigate = useNavigate()
+
+    const reqNumbers = "0123456789"
+    const reqChars = "abcdefghijklmnopqrstuvwxyz"
+    const reqCaps = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    const reqSymbols = "!@#?$%^&*()_-+="
+
+    const includesNumbers  = () => {
+        const numbersArray = reqNumbers.split("")
+        let result = false
+        numbersArray.forEach( ( Number) => {
+            if( password.includes(number) ) {
+                result = true
+            }
+        })
+        return result
+    }
+
+    const includesChars = () => {
+        const charsArray = reqChars.split("")
+        charsArray.forEach( (char) => {
+            if( password.includes(char) ) {
+                result = true
+            }
+        })
+        return result
+    }
+
+    const includesCaps = () => {
+        const capsArray = reqCaps.split("")
+        capsArray.forEach( (cap) => {
+            if( password.includes(cap) ) {
+                result = true
+            }
+        })
+        return result
+    }
+
+    const includesSymbols = () => {
+        const SymbolsArray = reqSymbols.split("")
+        let result = false
+        SymbolsArray.forEach( (symbol) => {
+            if( password.includes(symbol) ) {
+                result = true
+            }
+        })
+        return result
+    }
+    
+    useEffect ( () => {
+        if( 
+            (password.length >= 8 && password.length <= 15)
+            && includesNumbers() == true
+            && includesChars() == true
+            && includesCaps() == true
+            && includesSymbols() == true
+        
+        ) 
+        {
+            setValidPassword( true )
+        }
+        else {
+            setValidPassword( false )
+        }
+    } , [ password ] )
 
     const signUpUser = (event) => {
         event.preventDefault()
@@ -24,7 +92,7 @@ export function Signup(props) {
             <Container>
                 <Row>
                     <Col md={{ span: 4, offset: 4 }}>
-                        <Form className='mt-4' onSubmit={(event) => signUpUser(event)}>
+                        <Form className='mt-4' onSubmit={(event) => signUpUser(event) }>
                             <h2>Sign up for an account</h2>
                             <Form.Group>
                                 <Form.Label>Email</Form.Label>
@@ -40,15 +108,28 @@ export function Signup(props) {
                                     type='password'
                                     placeholder='minimum 6 characters'
                                     name='password'
+                                    value={password}
+                                    onChange={(event) => setPassword(event.target.
+                                        value)}
                                 />
-                            </Form.Group>
-                            <Button type='submit' variant='primary' className='my-3 mx-auto d-block w-50'>
-                                Sign up
-                            </Button>
-                        </Form>
-                    </Col>
-                </Row>
-            </Container>
-        </>
-    )
+                                <Form.Text>
+                                    Password must contain at least one uppercase,
+                                    lowercase, a number and a symbol and be between 8 and 
+                                    15 characters long
+                                </Form.Text>
+                                <Form.Group>
+                                    <Button
+                                        type='submit'
+                                        variant='primary'
+                                        className='my-3 mx-auto d-block w-100'
+                                        disabled={(validpassword) ? false : true}
+                                    >
+                                        Sign up
+                                    </Button>
+                                </Form.Group>
+                            </Col>
+                        </Row>
+                    </Container>
+                </>
+                )
 }
